b9ca65a072089ab38208506939e65bbf6128c313
MauF23 <m.fuentes@sae.edu>
2026-02-04 10:53:30 -0600

    Comportamiento Wander a enemigo

--- Assets/Proto/Scripts/EnemyBehaviourAndHealth.cs
+++ Assets/Proto/Scripts/EnemyBehaviourAndHealth.cs
@@ -7,6 +7,11 @@ public class EnemyBehaviourAndHealth : MonoBehaviour
     public Transform player;
     public NavMeshAgent navMesh;
 
+    private Vector3 startPosition; //la posición inicial del enemigo
+    private Vector3 currentDestination; //el destino que seguirá en el nav mesh
+
+    public bool aggro; // determinar si el enemigo es agresivo o no.
+
     //Enteros
     public int enemyHealth;
     public int enemyDamage;
@@ -14,6 +19,8 @@ public class EnemyBehaviourAndHealth : MonoBehaviour
     //Flotantes
     private float attackCooldown;
     private float attackTimer;
+    private float wanderPointsMaxTries = 5; //número de intentos máximos para obtener un punto aleatorio
+    public float wanderRadius = 15; // radio en el que buscará un punto aletorio
 
     void Start()
     {
@@ -23,12 +30,21 @@ public class EnemyBehaviourAndHealth : MonoBehaviour
         enemyDamage = 1;
         attackCooldown = 0;
         attackTimer = 0;
-    }
+		startPosition = transform.position;
+
+		UpdateDestination();
+	}
 
     void Update()
     {
-        navMesh.SetDestination(player.position);    
-        if (IsPlayerInHarmRange())
+        //navMesh.SetDestination(player.position);
+
+        if (!navMesh.pathPending && navMesh.remainingDistance <= navMesh.stoppingDistance)
+        {
+			UpdateDestination();
+		}
+
+		if (IsPlayerInHarmRange())
         {
             AttackPlayer();
         }
@@ -53,4 +69,78 @@ public class EnemyBehaviourAndHealth : MonoBehaviour
     {
         Debug.Log("Enemy has attacked player");
     }
+
+    /// <summary>
+    /// Actualiza el punto de navegación al que se moverá el enemigo, si esta aggro será la posición del jugador.
+    /// De lo contrario será un punto aleatorio.
+    /// </summary>
+    public void UpdateDestination()
+    {
+        if (aggro)
+        {
+			navMesh.SetDestination(player.position);
+            return;
+		}
+
+		navMesh.SetDestination(GetWanderPoint());
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		PlayerController player = other.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            aggro = true;
+            UpdateDestination();
+		}
+	}
+
+    /// <summary>
+    /// Obtiene un punto aleatorio al rededor del jugador especificado por su wanderRadius
+    /// </summary>
+	private Vector3 GetRandomPoint()
+	{
+		//crear una esfera con en radio específicado, esta siempre se crea en la posición Vector.zero del mudno
+		Vector3 randomPoint = Random.insideUnitSphere * wanderRadius;
+
+		//Para centrar la esfera a la posición del jugador, se le suma la posición de este
+		randomPoint += transform.position;
+		return randomPoint;
+	}
+
+
+	/// <summary>
+	/// Obtiene un punto aleatorio al que el enemigo se moverá
+	/// </summary>
+	/// <returns>un punto alcanzable en el navMesh o la posición inicial si no lo encuentra</returns>
+	private  Vector3 GetWanderPoint()
+	{
+		NavMeshHit hit; //variable similar a Raycasthit, es para verificar si un punto está sobre on navMesh
+		Vector3 finalPoint = startPosition; //variable que determinará la posición obtenida
+
+       
+        for (int i = 0; i < wanderPointsMaxTries; i++)
+        {
+            //Obtener un punto aleatorio al rededor del enemigo
+			Vector3 randomPoint = GetRandomPoint();
+
+			//SamplePosition verifica si el punto obtenido está dentro de un nav mesh
+			if (NavMesh.SamplePosition(randomPoint, out hit, wanderRadius, 1))
+            {
+                finalPoint = hit.position;
+                break; //si se obtiene un punto válido, detener el ciclo for.
+            }
+
+			//Si no se obtuvo un punto válido el for continúa intentando obtener otro. El número de intentos
+			//dependerá del valor de wanderPointsMaxTries.
+		}
+		return finalPoint;
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, wanderRadius);    
+	}
+
 }
