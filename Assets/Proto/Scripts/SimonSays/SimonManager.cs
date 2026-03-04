aa7632a33c9afd274c29d9888fb44612635c5d8e
Pedro-DJC <pedro.juarez@saei.mx>
2026-03-04 16:54:07 -0600

    Key_Locations_Updated
    Bgm added, Heirarchy organized, Simon puzzle fixed, Simon typography changed, drops names updated

--- Assets/Proto/Scripts/SimonSays/SimonManager.cs
+++ Assets/Proto/Scripts/SimonSays/SimonManager.cs
@@ -150,7 +150,7 @@ public class SimonManager : MonoBehaviour
             {
                 Debug.Log("Incorrec");
                 incorrectAudio.Play();
-                simonGameActive = false;
+                //simonGameActive = false;
             }
 
         }
